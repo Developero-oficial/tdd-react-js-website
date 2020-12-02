bf8ee82b402d595370416bc63d979d49f225e50f
juan-carlos-correa <juan.correa.herrera@gmail.com>
2020-12-01 19:16:58 -0600

    updated siteMetadata author

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `TDD con React JS`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Juan Correa`,
+      summary: `Full Stack software engineer.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
